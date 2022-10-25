f3a740f9474bae1537596c14aa785be8a74cf8e8
Muhammed Talha Ugurel <muhammedtalhaugurel@gmail.com>
2022-10-25 16:13:55 +0300

    whit permiton of ALLAH its done this time

--- ex00/ft_print_program_name.c
+++ ex00/ft_print_program_name.c
@@ -6,7 +6,7 @@
 /*   By: mugurel <muhammedtalhaugurel@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 00:53:59 by mugurel           #+#    #+#             */
-/*   Updated: 2022/10/25 01:04:09 by mugurel          ###   ########.fr       */
+/*   Updated: 2022/10/25 16:12:30 by mugurel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ void	ft_putstr(char *str)
 
 int	main(int ac, char **av)
 {
-	ft_putstr(av[0]);
+	if (ac >= 1)
+		ft_putstr(av[0]);
 	ft_putchar('\n');
 }
